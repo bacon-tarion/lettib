bccd464a1db77177c0e34180edd70a2f243027e8
bacon-tarion <rlbrazz@gmail.com>
2026-07-09 22:36:11 -0600

    gemini update 3

--- lib/stripe/client.ts
+++ lib/stripe/client.ts
@@ -12,7 +12,7 @@ export function getStripe(): Stripe {
 
   if (!stripeInstance) {
     stripeInstance = new Stripe(key, {
-      apiVersion: "2026-05-27.dahlia",
+      apiVersion: "2026-06-24.dahlia",
       typescript: true,
     });
   }
